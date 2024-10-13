50576feeecaddf129fd3622754e45f5bef43a57d
vinodibm05 <vinodibm05@gmail.com>
2024-10-13 19:43:12 +0530

    changes

--- src/test/java/com/springboot/myapp/MyAppApplicationTests.java
+++ src/test/java/com/springboot/myapp/MyAppApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MyApp2ApplicationTests {
+class MyAppApplicationTests {
 
 	@Test
 	void contextLoads() {
